0834d3f18b336e82422a2365a36512ebba68fc5e
Jagmeet <js2689314@gmail.com>
2021-10-23 16:47:57 +0530

    Started Nqueen questions

--- LevelUp/l001_Recursion/l002RecursionTrees.java
+++ LevelUp/l001_Recursion/l002RecursionTrees.java
@@ -194,12 +194,197 @@ public class l002RecursionTrees{
         }
         return count;
     }
+
+    //2d queen Set=======================================================================================
+    // tboxes = total Bpxes, tqn = total queen, qpsf = queen placed so far, bn =
+    // box_no,(solve by imaging like 1d but to find coordinate imagine 2d)
+    public static int queenCombination2D(boolean [][]boxes,int tqn,int bn,String ans){
+        if(tqn == 0){
+            System.out.println(ans);
+            return 1;
+        }
+        
+        int n=boxes.length, m = boxes[0].length,count =0 ;
+        for(int i=bn;i<n*m;i++){
+            int r = i / m;
+            int c = i % m;
+
+            if(!boxes[r][c]){
+                boxes[r][c] = true;
+                count += queenCombination2D(boxes,tqn - 1,i + 1,ans + "(" + r + ", " + c + ") ");
+                boxes[r][c] = false;
+            }
+        }
+        return count;
+    }
+
+    public static int queenPermutation2D(boolean [][]boxes,int tqn,String ans){
+        if(tqn == 0){
+            System.out.println(ans);
+            return 1;
+        }
+        
+        int n=boxes.length, m = boxes[0].length,count = 0 ;
+        for(int i = 0;i < n*m;i++){
+            int r = i / m;
+            int c = i % m;
+
+            if(!boxes[r][c]){
+                boxes[r][c] = true;
+                count += queenPermutation2D(boxes,tqn - 1,ans + "(" + r + ", " + c + ") ");
+                boxes[r][c] = false;
+            }
+        }
+        return count;
+    }
+
+    //Nqueen series===========================================================================
     
+    public static boolean isSafeToPlaceQueen(boolean [][]boxes,int x,int y){
+       int dir[][] ={{-1,1},{-1,0},{-1,-1},{0,-1}};   // for nqueenCombination_01()
+        // int[][] dir = { { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 } };   // for nqueenPermutation_01()
+
+        int n = boxes.length,m = boxes[0].length;
+        for(int d=0;d < dir.length;d++){
+            for(int rad=1;rad < Math.max(n,m);rad++){
+                int r = x + rad * dir[d][0];
+                int c = y + rad * dir[d][1];
+                if(r>=0 && c>=0 && r<n && c<m){
+                    if(boxes[r][c])
+                        return false;
+                }
+                else 
+                    break;
+            }
+        }
+        return true;
+    }
+
+    public static int nqueenCombination_01(boolean [][]boxes,int tqn,int idx,String ans){
+        if(tqn == 0){
+            System.out.println(ans);
+            return 1;
+        }    
+
+        int count = 0,n = boxes.length,m = boxes[0].length;
+        for(int i = idx;i<n*m;i++){
+            int r = i / m;
+            int c = i % m;
+            if(isSafeToPlaceQueen(boxes,r,c)){
+                boxes[r][c] = true;
+                count += nqueenCombination_01(boxes,tqn-1,i+1,ans + "(" + r + ", " + c + ") ");
+                boxes[r][c] = false;
+            }
+        }
+        return count;
+    }
+
+    public static int nqueenPermutation_01(boolean [][]boxes,int tqn,int idx,String ans){
+        if(tqn == 0){
+            System.out.println(ans);
+            return 1;
+        }    
+
+        int count = 0,n = boxes.length,m = boxes[0].length;
+        for(int i = idx;i<n*m;i++){
+            int r = i / m;
+            int c = i % m;
+            if(!boxes[r][c]){
+                if(isSafeToPlaceQueen(boxes,r,c)){
+                    boxes[r][c] = true;
+                    count += nqueenPermutation_01(boxes,tqn-1,0,ans + "(" + r + ", " + c + ") ");
+                    boxes[r][c] = false;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static int nqueenCombinationSubseq_02(boolean [][]boxes,int tqn,int idx,String ans){
+        int count=0,n = boxes.length,m = boxes[0].length;
+        if(idx >= n*m || tqn == 0){
+            if(tqn == 0){
+                System.out.println(ans);
+                return 1;
+            }
+            return 0; 
+        }
+        
+        int r = idx / m;
+        int c = idx % m; 
+        if(isSafeToPlaceQueen(boxes,r,c)){
+            boxes[r][c] = true;
+            count += nqueenCombinationSubseq_02(boxes,tqn-1,idx + 1,ans + "(" + r + ", " + c + ") ");
+            boxes[r][c] = false;
+        }
+        count += nqueenCombinationSubseq_02(boxes,tqn,idx + 1,ans);
+        return count;
+    }
+
+    //Using shadow technique...
+    static boolean rows[];
+    static boolean cols[];
+    static boolean Adiag[];
+    static boolean diag[];
+
+    static int calls=0;
+
+    public static int nqueenCombination_03(int tqn,int idx,int n,int m,String ans){
+        if(tqn == 0){
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        calls++;
+        for(int i =idx;i<n*m;i++){
+            int r = i / m;
+            int c = i % m;
+            if(!rows[r] && !cols[c] && !diag[r+c] && !Adiag[r-c + (m-1)]){
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + (m-1)] = true;
+
+               count += nqueenCombination_03(tqn-1,i+1,n,m,ans + "(" + r + ", " + c + ") ");
+               
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + (m-1)] = false;
+            }
+        }
+        return count;
+    }
+
+    public static int nqueenPermutation_03(int tqn,int idx,int n,int m,String ans){
+        if(tqn == 0){
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        calls++;
+        for(int i =idx;i<n*m;i++){
+            int r = i / m;
+            int c = i % m;
+            if(!rows[r] && !cols[c] && !diag[r+c] && !Adiag[r-c + m-1]){
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + m-1] = true;
+
+               count += nqueenPermutation_03(tqn-1,0,n,m,ans + "(" + r + ", " + c + ") ");
+               
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + m-1] = false;
+            }
+        }
+        return count;
+    }
 
     public static void main(String []args){
         int arr[]={2,3,5,7};
+        int n=4,m=4;
         boolean vis[] =new boolean[5];
         boolean tboxes[] = new boolean[6];
+        boolean boxes[][] = new boolean[4][4];
+
+
+        rows = new boolean[n];
+        cols = new boolean[m];
+        diag = new boolean[n + m - 1];
+        Adiag = new boolean[n + m - 1];
         // System.out.println(permutationWithInfi(arr,10,""));
         // System.out.println(combinationWithInfi(arr,10,0,""));
         // System.out.println(combinationWithSingle(arr,10,0,""));
@@ -208,6 +393,14 @@ public class l002RecursionTrees{
         // System.out.println(combinationWithSingle_Subseq(arr,10,0,""));
         // System.out.println(permutationWithSingle_Subseq(arr,10,0,""));
         // System.out.println(queenCombination(6,4,0,0,""));
-        System.out.println(queenPermutation(tboxes,4,0,""));
+        // System.out.println(queenPermutation(tboxes,4,0,""));
+        // System.out.println(queenPermutation2D(boxes,4,""));
+        // System.out.println(nqueenCombination_01(boxes,4,0,""));
+        // System.out.println(nqueenPermutation_01(boxes,4,0,""));
+        // System.out.println(nqueenCombinationSubseq_02(boxes,4,0,""));
+        // System.out.println(nqueenCombination_03(4,0,n,m,""));
+        System.out.println(nqueenPermutation_03(4,0,n,m,""));
+        System.out.println(calls);
+
     }
 }
--- LevelUp/l001_Recursion/questions.java
+++ LevelUp/l001_Recursion/questions.java
@@ -72,4 +72,302 @@ public class questions{
        Collections.sort(arr);
         return arr;
     }
+
+    //39
+    public static int combinationWithInfi(int arr[],int target,int idx,List<List<Integer>>ans,List<Integer>smallans){
+        if(target == 0){
+            List<Integer> base = new ArrayList<>(smallans);
+            ans.add(base);
+            return 1;
+        }
+        
+        int count=0;
+        for(int i=idx;i<arr.length;i++){
+            if(target - arr[i] >=0){
+                smallans.add(arr[i]);
+                count += combinationWithInfi(arr,target-arr[i],i,ans,smallans);
+                smallans.remove(smallans.size()-1);
+            }
+        }
+        return count;
+    }
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+         List<List<Integer>> ans = new ArrayList<>();
+         combinationWithInfi(candidates,target,0,ans,new ArrayList<>());
+         return ans;
+    }
+
+    // 40
+    public int combinationSum2(int[] arr, int tar, int idx, List<Integer> smallAns, List<List<Integer>> res) {
+        if (tar == 0) {
+            List<Integer> base = new ArrayList<>(smallAns);
+            res.add(base);
+            return 1;
+        }
+
+        int count = 0;
+        boolean[] vis = new boolean[51];
+        for (int i = idx; i < arr.length; i++) {
+            if (!vis[arr[i]] && tar - arr[i] >= 0) {
+
+                vis[arr[i]] = true;
+
+                smallAns.add(arr[i]);
+                count += combinationSum2(arr, tar - arr[i], i + 1, smallAns, res);
+                smallAns.remove(smallAns.size() - 1);
+            }
+        }
+
+        return count;
+    }
+
+    public int combinationSum2_01(int[] arr, int tar, int idx, List<Integer> smallAns, List<List<Integer>> res) {
+        if (tar == 0) {
+            List<Integer> base = new ArrayList<>(smallAns);
+            res.add(base);
+            return 1;
+        }
+
+        int count = 0;
+        int prev = -1;
+        for (int i = idx; i < arr.length; i++) {
+            if (prev != arr[i] && tar - arr[i] >= 0) {
+                smallAns.add(arr[i]);
+                count += combinationSum2_01(arr, tar - arr[i], i + 1, smallAns, res);
+                smallAns.remove(smallAns.size() - 1);
+            }
+
+            if (tar - arr[i] < 0)
+                break;
+
+            prev = arr[i];
+        }
+
+        return count;
+    }
+
+    public int combinationSum2_Subseq(int[] arr, int tar, int idx, List<List<Integer>> ans, List<Integer> smallAns) {
+        if (idx >= arr.length || tar == 0) {
+            if(tar == 0){
+                List<Integer> base = new ArrayList<>(smallAns);
+                ans.add(base);
+                return 1;
+            }
+            return 0;
+        }
+
+        int count =0;
+        if(tar - arr[idx] >=0){
+            smallAns.add(arr[idx]);
+            count += combinationSum2_Subseq(arr,tar-arr[idx],idx+1,ans,smallAns);
+            smallAns.remove(smallAns.size()-1);
+        }
+        idx++;
+        while(idx < arr.length && arr[idx] == arr[idx-1]){
+            idx++;
+        }
+        count += combinationSum2_Subseq(arr,tar,idx,ans,smallAns);
+
+        return count;
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);  //its required to sort to make widthwise blocked.
+        List<List<Integer>> ans = new ArrayList<>();
+         combinationSum2_Subseq(candidates,target,0,ans,new ArrayList<>());
+         return ans;   
+    }
+
+    //77
+    public static int combination(int n,int k,int idx,List<List<Integer>> ans,List<Integer> smallAns){
+        if(k == 0){
+            List<Integer> base = new ArrayList<>(smallAns);
+            ans.add(base);
+            return 1;
+        }
+        
+        int count=0;
+        for(int i =idx;i<=n;i++){
+            smallAns.add(i);
+            count += combination(n,k-1,i+1,ans,smallAns);
+            smallAns.remove(smallAns.size()-1);
+        }
+        return count;
+    }
+
+    //216
+    public static int combinationIII(int target,int k,int idx,List<List<Integer>> ans,List<Integer> smallAns){
+        if(k == 0 || target == 0){
+            if(k==0 && target ==0){
+                List<Integer> base = new ArrayList<>(smallAns);
+                ans.add(base);
+                return 1;
+            }
+            return 0;
+        }
+        
+        int count=0;
+        for(int i =idx;i<=9;i++){
+            if(target - i >=0){
+                smallAns.add(i);
+                count += combinationIII(target-i,k-1,i+1,ans,smallAns);
+                smallAns.remove(smallAns.size()-1);
+            }
+            else
+                break;
+        }
+        return count;
+    }
+    
+    public List<List<Integer>> combinationSumIII(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        combinationIII(n,k,1,ans,new ArrayList<>());
+        return ans;
+    }
+
+    //46
+        //tel = total elements.
+       public static int permute(int tel,int[] nums,List<List<Integer>> ans,List<Integer> smallAns){
+            if(tel == 0){
+                List<Integer> base = new ArrayList<>(smallAns);
+                ans.add(base);
+                return 1;
+            }
+            
+            int count=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i] != -11){  //-11 as per constraints.
+                    int val = nums[i];
+                    nums[i] = -11;
+                    smallAns.add(val);
+                    count += permute(tel-1,nums,ans,smallAns);
+                    smallAns.remove(smallAns.size()-1);
+                    nums[i] =val;
+                }
+                
+            }
+            return count;
+        }
+        
+        public List<List<Integer>> permute(int[] nums) {
+             List<List<Integer>> ans = new ArrayList<>();
+             permute(nums.length,nums,ans,new ArrayList<>());
+            return ans;
+        }
+
+    //47
+    public static int permuteUnique(int tel,int[] nums,List<List<Integer>> ans,List<Integer> smallAns) {
+        if(tel == 0){
+            List<Integer> base = new ArrayList<>(smallAns);
+            ans.add(base);
+            return 1;
+        }
+        
+        int prev=-12,count=0;       //-12 as per constraints
+        for(int i =0;i<nums.length;i++){
+            if(nums[i] > -11 && prev!=nums[i]){ //-11 as per constraints
+                int val = nums[i];
+                nums[i] = -11;
+                smallAns.add(val);
+                count += permuteUnique(tel-1,nums,ans,smallAns);
+                smallAns.remove(smallAns.size()-1);
+                nums[i] = val; 
+                prev = nums[i];
+            } 
+        }
+        return count;
+    }
+    
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>>  ans= new ArrayList<>();
+        permuteUnique(nums.length,nums,ans,new ArrayList<>());
+        return ans;
+    }
+
+    //784
+    public int letterCasePermutation(int idx,String s,List<String> res,String ans) {
+        if(idx == s.length()){
+            String base = new String(ans);
+            res.add(base);
+            return 1;
+        }
+        
+        int count=0;
+        char ch = s.charAt(idx);
+        if(ch >='0' && ch <='9')
+            count += letterCasePermutation(idx+1,s,res,ans); 
+        else{
+            ch = (char)((ch >= 'A' && ch <='Z')?(ch - 'A' + 'a') : (ch - 'a' + 'A')) ;
+            count += letterCasePermutation(idx+1,s,res,ans.substring(0,idx) + ch + ans.substring(idx+1,s.length()));       
+            count +=  letterCasePermutation(idx+1,s,res,ans);
+        }
+        return count;
+    }
+
+    //52 nqueen II
+    static boolean rows[];
+    static boolean cols[];
+    static boolean Adiag[];
+    static boolean diag[];
+
+    public static int nqueenCombination_03(int tqn,int idx,int n,int m,String ans){
+        if(tqn == 0){
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        for(int i =idx;i<n*m;i++){
+            int r = i / m;
+            int c = i % m;
+            if(!rows[r] && !cols[c] && !diag[r+c] && !Adiag[r-c + (m-1)]){
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + (m-1)] = true;
+
+               count += nqueenCombination_03(tqn-1,i+1,n,m,ans + "(" + r + ", " + c + ") ");
+               
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + (m-1)] = false;
+            }
+        }
+        return count;
+    }
+
+
+    //51 nqueen
+    public static int nqueenCombination_03(boolean [][]boxes,int tqn,int idx,int n,int m,List<List<String>> res){
+        if(tqn == 0){
+            List<String> smallAns = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j=0;j<m;j++){
+                    if(boxes[i][j])
+                        sb.append("Q");
+                    else
+                        sb.append(".");
+                }
+                smallAns.add(sb.toString());
+            }
+            res.add(smallAns);
+            return 1;
+        }
+
+        int count = 0;
+        calls++;
+        for(int i =idx;i<n*m;i++){
+            int r = i / m;
+            int c = i % m;
+            if(!rows[r] && !cols[c] && !diag[r+c] && !Adiag[r-c + (m-1)]){
+                boxes[r][c] =true;
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + (m-1)] = true;
+
+               count += nqueenCombination_03(boxes,tqn-1,i+1,n,m,res);
+               
+               rows[r] = cols[c] = diag[r+c] = Adiag[r-c + (m-1)] = false;
+                boxes[r][c] =false;
+            }
+        }
+        return count;
+    }
+
 }
